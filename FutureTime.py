dc7f93cb7e4d8a373f800e2867550d0b261eb95a
LukeS190 <48619535@nebraska.edu>
2025-09-01 16:08:26 +0000

    Lab 2 done

--- FutureTime.py
+++ FutureTime.py
@@ -1,7 +1,7 @@
 #FutureTime.py
-#Name:
-#Date:
-#Assignment:
+#Name:Luke Sheardown
+#Date:9-1-2025
+#Assignment:Lab 2
 
 # datetime will allow us to access the system date and time.
 import datetime
@@ -9,14 +9,23 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
-  print (currentHour, currentMinute) #this is just for checking, we should delete it later
+
+   #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
+  moreHours = 2
   #Ask user for minutes
+  moreMins = 25 
+
+futureMins = (currentMinute + moreMins ) % 60
+extraHour = (currentMinute + moreMins ) // 60
+print(extraHour)
+
+print(futureMins)
 
   #Calculate the time after the user-supplied time has passed.
 
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name:Luke Sheardown
+#Date:9-1-2025
+#Assignment:Lab 2
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,17 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+  What is my name
+answers = ["Luke 1", "Jeff 2", "Mike 3", "Taylor 4", "Gordon 5"]
   #Answer question randomly with one of the options from your earlier list.
+r = random.random() * 5
 
+
+r = int(r) #cut off any decimal values
+
+print(r)
+response = answers[r]
+print(response)
 
 if __name__ == '__main__':
   main()
